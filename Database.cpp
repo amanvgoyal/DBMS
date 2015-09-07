79688dd4b2591cddcf49af1f0caa818963746d4e
Ronilo Ragodos <roniloragodos57@linux2.cs.tamu.edu>
2015-09-07 18:44:47 -0500

    Updated Skeleton, added makefile

--- Database.cpp
+++ Database.cpp
@@ -1,10 +1,16 @@
 #include "Database.h"
 #include <iostream>
+
 using namespace std;
+
+Database::Database() {
+  // ...
+}
 
 void Database::selection(){
 	
 }
+
 void Database::projection(){
 
 }
@@ -20,6 +26,44 @@ void Database::set_union(){
 void Database::set_diff(){
 
 }
+
 void Database::cross_product(){
 
 }
+
+void Database::open() {
+
+}
+
+void Database::close() {
+
+}
+
+void Database::save() {
+
+}
+
+void Database::exit() {
+
+}
+
+void Database::show() {
+
+}
+
+void Database::create() {
+
+}
+
+void Database::update() {
+
+}
+
+void Database::insert() {
+
+}
+
+void Database::delete_tuple() {
+
+}
+
